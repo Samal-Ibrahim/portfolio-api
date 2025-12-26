409c0be41f553c9e21b96416f81624c0e8f46e9c
Samal <56509582+FransoArbela@users.noreply.github.com>
2025-12-26 14:59:30 +0100

    Delete dist directory

--- dist/auth/auth.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("./auth.controller");
-const router = (0, express_1.Router)();
-router.post("/login", auth_controller_1.login);
-router.get("/me", auth_controller_1.me);
-router.post("/logout", auth_controller_1.logout);
-exports.default = router;
